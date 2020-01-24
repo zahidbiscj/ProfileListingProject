879b205cb48f3610d74ef9ae62f976720a69e4b2
Zahid Ahmed <zahidbiscj@gmail.com>
2020-01-24 21:32:15 +0600

    HomePage Company Added

---
+++ ProfileListingProject/ProfileListingProject.Web/Controllers/CompanyController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProfileListingProject.Web.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+    }
+}
